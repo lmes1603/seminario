90800e95ca027d41e20419e46b2ba5530d94e322
Luis Miguel Escalante Sanchez <luis.92.m@gmail.com>
2014-11-27 09:28:45 -0430

    sw

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ server.configure(function(){
 });
 
 server.get('/',function(req,res){
-	res.send('hola, Mundo');	
+	res.send('Servicio Web Seminario <br> Luis Escalante <br> Ingrid Lazaro <br> Manuel Arevanos <br> Aztiley Araque <br> Jose Paz'
+		+ '<br>app.get(/Usuarios,findAllusuario) <br>app.get(/Usuario/:id,usuariofindById) <br>app.post(/Usuario,addUsuario) <br>app.put(/Usuario/:id,updateusuario) <br>app.delete(/Usuario/:numero_telefono, deleteusuario)<br><br>		app.get(/eventos,findAlleventos)<br>app.get(/evento/:id,eventofindById)<br>app.post(/evento,addevento)<br>app.put(/evento/:id,updateeventos)<br>app.delete(/evento/:numero_telefono, deleteeventos)<br><br>		app.get(/historials,findAllhistorial)<br>app.get(/historial/:id,historialfindById)<br>app.post(/historial,addhistorial)<br>app.put(/historial/:id,updatehistorial)<br>app.delete(/historial/:numero_telefono, deletehistorial)<br><br>app.get(/mensajess,findAllmensajes) <br>app.get(/mensajes/:id,mensajesfindById)<br>app.post(/mensajes,addmensajes)<br>app.put(/mensajes/:id,updatemensajes)<br>app.delete(/mensajes/:numero_telefono, deletemensajes)<br><br>		app.get(/mensajehistoricos,findAllmensajehistorico)<br>app.get(/mensajehistorico/:id,mensajehistoricofindById)<br>app.post(/mensajehistorico,addmensajehistorico)<br>app.put(/mensajehistorico/:id,updatemensajehistorico)<br>app.delete(/mensajehistorico/:numero_telefono, deletemensajehistorico)<br>'
+
+		);	
 });
 
 
