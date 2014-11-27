2dd23a2caf16ab75908a2aab5b25686fd4aa225c
Luis Miguel Escalante Sanchez <luis.92.m@gmail.com>
2014-11-27 02:34:51 -0430

    sw

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 var express = require ('express');
 var mongoose = require ('mongoose');
-var app = express();
+var server = express();
 
 mongoose.connect('mongodb://lmes1603:lumiessa92@ds051110.mongolab.com:51110/asistente', function(err,res){
 	if(err) console.log('ERROR Conectando a la BD: '+ err);
 	else console.log('Conexon a la BD realizada');
 });
 
-app.configure(function(){
-	app.use(express.bodyParser());
-	app.use(express.methodOverride()),
-	app.use(app.router);
+server.configure(function(){
+	server.use(express.bodyParser());
+	server.use(express.methodOverride()),
+	server.use(server.router);
 });
 
-app.get('/',function(req,res){
+server.get('/',function(req,res){
 	res.send('hola, Mundo');	
 });
 
-require('./routes')(app);
 
-app.listen(5000);
-console.log('escuchando por el puerto 5000');
+require('./routes')(server);
+
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( 'Listening on ' + server_ip_address + ', server_port ' + server_port );
+});
